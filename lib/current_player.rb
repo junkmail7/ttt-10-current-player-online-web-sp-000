cc26251df5d555c755e978202e3d9f5ef6dd2e59
William Pannell <lpann5@gmail.com>
2020-10-30 21:42:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,24 +2,16 @@ def turn_count(board)
   turns = 0
   board.each do |x|
     if x == "X" || x== "O"
-<<<<<<< HEAD
       turns += 1
-=======
-      turn = turn + 1
->>>>>>> 796b7d51879290de3272820a925bbbd7d2508c28
     end
   end
   return turns
 end
 
-<<<<<<< HEAD
 def current_player(board)
   if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
   end
-=======
-def current_player
->>>>>>> 796b7d51879290de3272820a925bbbd7d2508c28
 end
